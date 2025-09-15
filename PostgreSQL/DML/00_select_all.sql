77d7f83d58f90b41b523ac7da41448b1edbc7c71
DiyorIsa <james.findustry@gmail.com>
2025-09-15 13:08:12 +0500

    create DML folder with corresponding file

---
+++ PostgreSQL/DML/00_select_all.sql
@@ -0,0 +1,7 @@
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM projects;
+SELECT * FROM completed_projects;
+SELECT * FROM project_assignments;
+SELECT * FROM head_shots;
+SELECT * FROM upcoming_projects;
